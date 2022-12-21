d7c04b978957eb080b72fdb5c2f71c91ab8b40a6
Tri Nguyen <nnt@leapslabs.com>
2022-12-21 20:22:37 +0700

    SDCard: Unmount sdcard if it is already mounted

--- lib/sd_card/src/SDCard.cpp
+++ lib/sd_card/src/SDCard.cpp
@@ -19,20 +19,24 @@ extern bool gMountedSDCard;
 
 SDCard::SDCard(const char *mount_point, gpio_num_t miso, gpio_num_t mosi, gpio_num_t clk, gpio_num_t cs)
 {
-  //newTom
-#if 0
-  gMountedSDCard=false;
-  esp_vfs_fat_sdmmc_unmount();
-#endif
-  //End newTom
+  sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+  spi_bus_config_t bus_cfg = {
+      .mosi_io_num = mosi,
+      .miso_io_num = miso,
+      .sclk_io_num = clk,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+      .max_transfer_sz = 4000,
+  };
+  esp_err_t ret;
 
   if (gMountedSDCard) {
-    ESP_LOGI(TAG, "SD Card already mounted");
-    return;
+    esp_vfs_fat_sdmmc_unmount();
   }
 
+  gMountedSDCard = false;
   m_mount_point = mount_point;
-  esp_err_t ret;
+  
   // Options for mounting the filesystem.
   // If format_if_mount_failed is set to true, SD card will be partitioned and
   // formatted in case when mounting fails.
@@ -56,33 +60,23 @@ SDCard::SDCard(const char *mount_point, gpio_num_t miso, gpio_num_t mosi, gpio_n
       .allocation_unit_size = 32 * 1024};  //was 16
 
   ESP_LOGI(TAG, "Initializing SD card");
+    
+  ret = spi_bus_initialize((spi_host_device_t)host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+  if ((ret != ESP_OK) && (ret != ESP_ERR_INVALID_STATE)) {
+      ESP_LOGE(TAG, "Failed to initialize bus.");
+      return;
+  }
 
-  sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+  // This initializes the slot without card detect (CD) and write protect (WP) signals.
+  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+  sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+  slot_config.gpio_cs = cs;
+  slot_config.host_id = (spi_host_device_t)host.slot;
 
-    spi_bus_config_t bus_cfg = {
-        .mosi_io_num = mosi,
-        .miso_io_num = miso,
-        .sclk_io_num = clk,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 4000,
-    };
-    ret = spi_bus_initialize((spi_host_device_t)host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to initialize bus.");
-        return;
-    }
-
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
-    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
-    slot_config.gpio_cs = cs;
-    slot_config.host_id = (spi_host_device_t)host.slot;
-
-    host.max_freq_khz = 18000;
+  host.max_freq_khz = 18000;
 
 
-    ret = esp_vfs_fat_sdspi_mount(m_mount_point.c_str(), &host, &slot_config, &mount_config, &m_card);
+  ret = esp_vfs_fat_sdspi_mount(m_mount_point.c_str(), &host, &slot_config, &mount_config, &m_card);
 
   if (ret != ESP_OK)
   {
